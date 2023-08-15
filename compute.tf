525f68013dbed464a4323ccae01a6cff673c52bf
Yannis Tsopokis <tsopokis@users.noreply.github.com>
2023-08-15 16:20:00 +0300

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -5,7 +5,7 @@ data "oci_identity_availability_domains" "ads" {
 data "oci_core_images" "ampere-ubuntu-images" {
   compartment_id           = var.oci_compartment_id
   operating_system         = "Canonical Ubuntu"
-  operating_system_version = "20.04"
+  operating_system_version = "22.04"
   shape                    = "VM.Standard.A1.Flex"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
@@ -22,8 +22,6 @@ resource "oci_core_instance" "oracle-arm" {
   }
   source_details {
     boot_volume_size_in_gbs = "200"
-    # Platform Image: Ubuntu 20.04
-    # source_id   = "ocid1.image.oc1.iad.aaaaaaaa2tex34yxzqunbwnfnat6pkh2ztqchvfyygnnrhfv7urpbhozdw2a"
     source_id   = data.oci_core_images.ampere-ubuntu-images.images[0].id
     source_type = "image"
   }
